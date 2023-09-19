7d97c1ec2ee8fb57d35a754ce4e282786f9ab078
Gulce Gumrukcu <gumrukcugulce@gmail.com>
2023-09-19 18:09:16 +0300

    kirik img x2

--- src/components/lists/HeaderInputLists.jsx
+++ src/components/lists/HeaderInputLists.jsx
@@ -12,10 +12,10 @@ function HeaderInputLists() {
 
                     <input placeholder="Liste Ara" className="bg-[#F0F3F5] placeholder:pl-8 placeholder:text-sm placeholder:text-gray-800 focus:border-[#62a1ff] px-4 w-[400px] rounded-full h-10" type="text" />
                     <div className="hover:bg-[#F0F3F5] p-2 mr-4 rounded-full cursor-pointer items-center flex justify-center">
-                        <img className="w-4 h-4.5  " src="/public/img/navigation/icon-list.png" alt="" />
+                        <img className="w-4 h-4.5  " src="/img/navigation/icon-list.png" alt="" />
                     </div>
                     <div className="hover:bg-[#F0F3F5] p-2 mr-4 rounded-full cursor-pointer items-center flex justify-center">
-                        <img className="w-3.5 h-3.5  " src="/public/img/profile/icon-settings.png" alt="" />
+                        <img className="w-3.5 h-3.5  " src="/img/profile/icon-settings.png" alt="" />
                     </div>
 
                 </div>
